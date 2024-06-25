5eab0c330f10ceba2c5e9b082640564064ba8c85
clauanher <candinahernandez@gmail.com>
2024-06-25 21:08:28 +0100

    constellation&header

--- bwstars/src/components/Header/Header.jsx
+++ bwstars/src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
-import { Link } from 'react-router-dom'
-import './Header.css'
+import { Link, useLocation } from 'react-router-dom'
 import { AppBar, Button } from '@mui/material'
 
-const options= [ "Home", "Profile" ]
+const options= [ "Home", "Profile", "Stars", "Constellations" ]
 
 function Header() {
+  const location = useLocation()
   return (
     <AppBar  position = 'sticky' sx={{display:"flex", flexDirection:"row"}}>
         {options.map((option, idx)=> {
         return <Link to={option} key={idx}>
-        <Button sx ={{color:"White"}}> 
+        <Button sx ={{color:"White"}} disabled={location.pathname.includes(option)}>
         {option}
         </Button>
         </Link>
